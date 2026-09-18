180c7669e25369d177d84d069661552f531d4192
Sanchit Gupta <sanchit221206@gmail.com>
2026-09-18 14:44:53 +0530

    fix production upload directory

--- server/src/middleware/upload.middleware.ts
+++ server/src/middleware/upload.middleware.ts
@@ -1,12 +1,19 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const uploadDir = path.resolve(process.cwd(), "uploads");
+
+fs.mkdirSync(uploadDir, {
+  recursive: true,
+});
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
+  destination: function (_req, _file, cb) {
+    cb(null, uploadDir);
   },
 
-  filename: function (req, file, cb) {
+  filename: function (_req, file, cb) {
     const uniqueName =
       Date.now() + "-" + Math.round(Math.random() * 1e9);
 
@@ -14,27 +21,22 @@ const storage = multer.diskStorage({
   },
 });
 
-
 const fileFilter = (
-  req: Express.Request,
+  _req: Express.Request,
   file: Express.Multer.File,
   cb: multer.FileFilterCallback
 ) => {
-
-  if(file.mimetype === "application/pdf"){
-    cb(null,true);
-  }
-  else{
+  if (file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
     cb(new Error("Only PDF files allowed"));
   }
-
 };
-
 
 export const upload = multer({
   storage,
   fileFilter,
-  limits:{
-    fileSize:10*1024*1024
-  }
+  limits: {
+    fileSize: 10 * 1024 * 1024,
+  },
 });
