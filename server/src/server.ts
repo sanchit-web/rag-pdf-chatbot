2a1112ad29012b0b4b5046438f42ed72a31f64aa
Sanchit Gupta <sanchit221206@gmail.com>
2026-09-18 13:49:15 +0530

    register document routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import chatRoutes from "./routes/chat.routes.js";
 import conversationRoutes from "./routes/conversation.routes.js";
+import documentRoutes from "./routes/document.routes.js";
 
 import app from "./app.js";
 
@@ -8,7 +9,7 @@ const port = Number(process.env.PORT) || 5000;
 
 app.use("/api/chat", chatRoutes);
 app.use("/api/conversations", conversationRoutes);
-
+app.use("/api/documents", documentRoutes);
 
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
